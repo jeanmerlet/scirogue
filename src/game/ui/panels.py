613c309b98f9d9d40257cb51d0a9999121573d24
jeanmerlet <jean.joseph.merlet@gmail.com>
2026-07-18 14:08:46 -0400

    Show equipped hands in sidebar

--- src/game/ui/panels.py
+++ src/game/ui/panels.py
@@ -6,7 +6,7 @@ def _stat_print(term, x, y, text):
 
 
 class SidebarPanel:
-    def render(self, term, rect, player_stats):
+    def render(self, term, rect, player_display):
         font = "sidebar"
         txt_col = "light blue"
         att_col = "light blue"
@@ -17,42 +17,46 @@ class SidebarPanel:
         x, y = rect.x + 1, rect.y + 1
         _stat_print(
             term, x, y,
-            f"[color={txt_col}]Health:[color={val_col}] {player_stats.hp}/{player_stats.max_hp} "
+            f"[color={txt_col}]Health:[color={val_col}] {player_display.hp}/{player_display.max_hp} "
         )
-        draw_bar(term, font, x+7, y, rect.w-9, player_stats.hp,
-                 player_stats.max_hp, "dark red")
+        draw_bar(term, font, x+7, y, rect.w-9, player_display.hp,
+                 player_display.max_hp, "dark red")
         y += 1
         _stat_print(
             term, x, y,
-            f"[color={txt_col}]Oxygen:[color={val_col}] {player_stats.oxy}/{player_stats.max_oxy} "
+            f"[color={txt_col}]Oxygen:[color={val_col}] {player_display.oxy}/{player_display.max_oxy} "
         )
-        draw_bar(term, font, x+7, y, rect.w-9, player_stats.oxy,
-                 player_stats.max_oxy, "dark cyan")
+        draw_bar(term, font, x+7, y, rect.w-9, player_display.oxy,
+                 player_display.max_oxy, "dark cyan")
         y += 2
         _stat_print(
             term, x, y,
-            f"[color={att_col}]AW:[color={val_col}] {player_stats.awareness:<4}"
-            f"[color={att_col}]EQ:[color={val_col}] {player_stats.equilibrium:<4}"
-            f"[color={att_col}]RE:[color={val_col}] {player_stats.reasoning:<4}"
-            f"[color={att_col}]VI:[color={val_col}] {player_stats.vigor}"
+            f"[color={att_col}]AW:[color={val_col}] {player_display.awareness:<4}"
+            f"[color={att_col}]EQ:[color={val_col}] {player_display.equilibrium:<4}"
+            f"[color={att_col}]RE:[color={val_col}] {player_display.reasoning:<4}"
+            f"[color={att_col}]VI:[color={val_col}] {player_display.vigor}"
         )
         y += 1
         _stat_print(
             term, x, y,
-            f"[color={def_col}]AV:[color={val_col}] {player_stats.armor:<4}"
-            f"[color={def_col}]EV:[color={val_col}] {player_stats.evasion:<4}"
-            f"[color={xp_col}]XL:[color={val_col}] {player_stats.xp_level:<4}"
-            f"[color={xp_col}]XP:[color={val_col}] {player_stats.xp_percent}%"
+            f"[color={def_col}]AV:[color={val_col}] {player_display.armor:<4}"
+            f"[color={def_col}]EV:[color={val_col}] {player_display.evasion:<4}"
+            f"[color={xp_col}]XL:[color={val_col}] {player_display.xp_level:<4}"
+            f"[color={xp_col}]XP:[color={val_col}] {player_display.xp_percent}%"
         )
         y += 2
         _stat_print(
             term, x, y,
-            f"[color={txt_col}]AV:[color={val_col}] {player_stats.armor:<4}"
+            f"[color={txt_col}]Main: "
+            f"[color={player_display.main_hand.color}]"
+            f"{player_display.main_hand.name}"
         )
         y += 1
         _stat_print(
             term, x, y,
-            f"[color={txt_col}]AV:[color={val_col}] {player_stats.armor:<4}"
+            f"[color={txt_col}]Offhand: "
+            f"[color={player_display.offhand.color}]"
+            f"{player_display.offhand.name}"
         )
 
 class LogPanel:
